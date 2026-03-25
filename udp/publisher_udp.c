a0d8de1d7150d829fff5737999f0df466712739a
Diego Alejandro Munévar Pérez <d.munevar@uniandes.edu.co>
2026-03-24 20:45:57 -0500

    Y por eso es que solo va a correr en linux xd

---
+++ udp/publisher_udp.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+
+#define BROKER_IP "127.0.0.1"
+#define BROKER_PORT 12345
+
+int main() {
+    printf("El publicador de UDP está funcionando...\n");
+    while (1){
+        
+    }
+}
---
+++ udp/subscriber_udp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BROKER_IP "127.0.0.1"
+#define BROKER_PORT 12345
+
+int main() {
+    printf("El suscriptor de UDP está funcionando...\n");
+    while (1){
+        
+    }
+}
